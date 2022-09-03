ba31a0370da469a0f368b7a8ddd4837a01cd582e
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-03 17:09:41 +0900

    프로젝트 생성

---
+++ mainproject/src/test/java/com/mainproject/ateam/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainproject.ateam.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
