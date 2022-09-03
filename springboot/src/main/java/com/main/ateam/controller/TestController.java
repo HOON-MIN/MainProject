7e484407cf2e9f4a714b869b3408f25d4b0208a1
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-03 18:39:49 +0900

    프론트엔드 공간

--- springboot/src/main/java/com/main/ateam/controller/TestController.java
+++ springboot/src/main/java/com/main/ateam/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.main.ateam.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	@GetMapping("/hello")
-	public String hello(String name) {
-		return "hello"+name;
+    public String hello(){        
+		return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다.";
 	}
 }
 	
