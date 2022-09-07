db63238f16b42e94fa35514dcada220d3582a923
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-07 20:45:41 +0900

    리액트 수정

--- springboot/frontend/src/App.js
+++ springboot/frontend/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState , useEffect } from "react";
 import Axios from 'axios';
 import "./App.css";
-import Memberlist from "./component/data/member";
 
 function App() {  
   const [users,setUsers] =useState();
   useEffect(() => {
-  Axios.get('/memberlist').then((response) => {
+  Axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
     if(response.data){
+      
       setUsers(response.data);
     }else{
       alert("failed to");
@@ -17,11 +17,13 @@ function App() {
 return (
 <div>
   <ul>
-    <li>{users.id}</li>
-    <li>{users.name}</li>
-  </ul>
-    
-  
+    <li>안녕</li>
+    { users.map(function(username){
+      return(
+        <li>{users.username}</li>
+      )
+    })}
+  </ul>  
 </div>
 )
 };
