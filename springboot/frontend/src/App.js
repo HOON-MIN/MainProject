0e885ed433ed75d825842e87d26bfef6dfdf4a8b
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-07 17:27:46 +0900

    리액트관련수정

--- springboot/frontend/src/App.js
+++ springboot/frontend/src/App.js
@@ -1,79 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState , useEffect } from "react";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/header";
 import Login from "./component/login";
-import Memberlist from "./component/data/member";
 import axios from 'axios';
+import Memberlist from "./component/data/member";
 
-function App() {
-  const [date, setDate] = useState([]);
-  const [memberlist, setMemberlist] = useState([]);
-  //const [users, setUsers] = useState();
- 
-
-  axios({
-    url : '/memberlist'
-  });
-  
-  // useEffect(() => {
-  //     fetch("/memberlist")
-  //       .then((response) => response.text())
-  //       .then((memberlist) => {
-  //         setMemberlist(memberlist);
-  //       });
-  //       console.log(memberlist ,"데이터타입 =>", typeof memberlist);
-  //   },[]);
-
-  // useEffect(() => {
-  //   axios({
-  //     method: "GET",
-  //     url: "http://localhost:83/memberlist",
-  //   }).then((response) => setUsers(response.data));
-  // });
-
-
-  useEffect(() => {
-    fetch("/memberlist")
-      .then((response) => response.text())
-      .then((memberlist) => {
-        setMemberlist(memberlist);
-      });
-      console.log(memberlist , typeof memberlist);
-  }, []);
-
-  // useEffect(() => {
-  //   fetch("/hello")
-  //     .then((response) => response.text())
-  //     .then((date) => {
-  //       setDate(date);
-  //     });
-  //     console.log(date);
-  // }, []);
-
-
-  // const result = memberlist.map((list) => <li key={list.num}> {list.name} : {list.tel} </li>)
+function App() {  
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-            </header>
-          }
-        ></Route>
+    <div>
+      <Memberlist/>
+    </div>
 
-        <Route path="/date" element={<span>{date}</span>}></Route>
-        <Route path="/memberlist" element={<p>{memberlist}</p>}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
   );
-}
-
+};
 export default App;
--- springboot/src/main/java/com/main/ateam/member/controller/MemberController.java
+++ springboot/src/main/java/com/main/ateam/member/controller/MemberController.java
@@ -31,24 +31,24 @@ public class MemberController {
 	@RequestMapping(value = "/memberlist",produces = "application/json;charset=utf-8")
 	public Object memberList(){
 		List<MemberVO> mlist = memberService.getList();
-		List<Map<String,Object>> list2 = new ArrayList<>();
-		System.out.println(mlist);
-		for (MemberVO e : mlist) {
-			Map<String, Object> map = new HashMap<>();
-			map.put("num", e.getNum());
-			map.put("name", e.getName());
-			map.put("tel", e.getTel());
-			map.put("ssn", e.getSsn());
-			list2.add(map);
-		}
-		String result = null;
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			result = mapper.writeValueAsString(list2);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		List<Map<String,Object>> list2 = new ArrayList<>();
+//		System.out.println(mlist);
+//		for (MemberVO e : mlist) {
+//			Map<String, Object> map = new HashMap<>();
+//			map.put("num", e.getNum());
+//			map.put("name", e.getName());
+//			map.put("tel", e.getTel());
+//			map.put("ssn", e.getSsn());
+//			list2.add(map);
+//		}
+//		String result = null;
+//		ObjectMapper mapper = new ObjectMapper();
+//		try {
+//			result = mapper.writeValueAsString(list2);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
-		return result;
+		return mlist;
 	}
 }
