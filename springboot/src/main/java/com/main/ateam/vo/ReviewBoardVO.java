e1fe1fb83e359f9d171f4b6178b5b7b2830e3011
DESKTOP-8428NHR\yojip <yojip2@naver.com>
2022-09-07 19:09:17 +0900

    리뷰 VO 추가

---
+++ springboot/src/main/java/com/main/ateam/vo/ReviewBoardVO.java
@@ -0,0 +1,19 @@
+package com.main.ateam.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewBoardVO {
+
+	int rnum;
+	String id;
+	String title;
+	String cont;
+	String udate;
+	int hits;
+	int likes;
+	String hname;
+	String dname;
+}
