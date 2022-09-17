3e69b938d52e22b327520473c14486422d5eedb8
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-17 22:54:50 +0900

    리액트 카테고리 확인

--- springboot/frontend/src/component/Categories.js
+++ springboot/frontend/src/component/Categories.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Categories = (props) => {
-  console.log("카테고리", props);
-  return <div></div>;
+  const [category, setCategory] = useState([]);
+  const test = [];
+  props.doctor.map((majors) => {
+    test.push(majors.dmajor);
+  });
+  const uniqueTest = test.filter(
+    (category, index) => test.indexOf(category) === index
+  );
+  console.log(uniqueTest);
+
+  {
+    uniqueTest.map((category, index) => console.log(index));
+  }
+
+  return (
+    <ul className="nav nav-tabs justify-content-center">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">
+          모든 진료과
+        </a>
+      </li>
+
+      {uniqueTest.map((category, index) => (
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            {category}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Categories;
