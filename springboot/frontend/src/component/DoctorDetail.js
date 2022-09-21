48e342dfb7a7e60b849454210a22015deb68d297
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-21 23:35:24 +0900

    리액트 수정 (모바일 화면 구현)

--- springboot/frontend/src/component/DoctorDetail.js
+++ springboot/frontend/src/component/DoctorDetail.js
@@ -5,76 +5,75 @@ const DoctorDetail = (props) => {
   console.log(props.doctor);
 
   return (
-    <div className="d-flex flex-column align-items-center p">
-      <div className="text-center">
+    <div>
+      <div className="text-center detail-img">
         <img src={DoctorDetailImg} className="detailImg" alt="..." />
       </div>
-      <div className="card detailCard border-0 p-2">
-        <div className="card-body ">
-          <div className="detailStatus">
-            <strong>현재 진료가능</strong>
+      <div className="d-flex justify-content-center ">
+        <div className="card border-0 p-2">
+          <div className="card-body ">
+            <div className="detailStatus">
+              <strong>현재 진료가능</strong>
+            </div>
+            <h5 className="card-title">
+              <strong> {props.doctor.dname} 의사</strong>
+            </h5>
+            <span className="detail-card-text">
+              {props.doctor.hospitalVO[0].hname}
+            </span>
+            <span className="detail-card-text">
+              　리뷰 : 1,045 <i className="bi bi-chevron-double-right"></i>
+            </span>
+            <div className="mt-2 d-flex flex-start">
+              <button className="btn btn-primary me-1 disabled" type="button">
+                {props.doctor.dmajor}
+              </button>
+            </div>
           </div>
-          <h5 className="card-title">
-            <strong> {props.doctor.dname} 의사</strong>
-          </h5>
-          <span className="detail-card-text">
-            {props.doctor.hospitalVO[0].hname}
-          </span>
-          <span className="detail-card-text">
-            　리뷰 : 1,045 <i className="bi bi-chevron-double-right"></i>
-          </span>
-          <div className="mt-2 d-flex flex-start">
-            <button className="btn btn-primary me-1 disabled" type="button">
-              {props.doctor.dmajor}
-            </button>
+          <div className="card-body border-top border-bottom ">
+            <p>
+              <strong>진료시간　</strong>
+              {props.doctor.hospitalVO[0].otime} ~{" "}
+              {props.doctor.hospitalVO[0].ctime}
+            </p>
+            <p>
+              <strong>점심시간　</strong>13:00 ~ 14:00
+            </p>
           </div>
-        </div>
-        <div className="card-body border-top border-bottom ">
-          <p>
-            <strong>진료시간　</strong>
-            {props.doctor.hospitalVO[0].otime} ~{" "}
-            {props.doctor.hospitalVO[0].ctime}
-          </p>
-          <p>
-            <strong>점심시간　</strong>13:00 ~ 14:00
-          </p>
-        </div>
-        <div className="card-body border-bottom">
-          <p>
-            <strong>진료과목</strong> {props.doctor.dmajor}
-          </p>
-          <p>
-            <strong>약력　　</strong> {props.doctor.dmajor}
-          </p>
-        </div>
-        <div className="card-body border-bottom">
-          <h6>
-            <strong>의사소개</strong>
-          </h6>
-          <div className="card-text">
-            안녕하세요 {props.doctor.dname} 의사입니다.
-            <br /> 친절하고 꼼꼼하게 진료하겠습니다. <br />
-            *다이어트 포함 비급여 항목 확인해주세요
+          <div className="card-body border-bottom">
+            <p>
+              <strong>진료과목</strong> {props.doctor.dmajor}
+            </p>
+            <p>
+              <strong>약력　　</strong> {props.doctor.dmajor}
+            </p>
           </div>
-        </div>
-        <div className="card-body border-bottom">
-          <h6>
-            <strong>병원위치</strong>
-          </h6>
-          <div className="card-text">{props.doctor.hospitalVO[0].hloc}</div>
-          <div className="detailMap"></div>
+          <div className="card-body border-bottom">
+            <h6>
+              <strong>의사소개</strong>
+            </h6>
+            <div className="card-text">
+              안녕하세요 {props.doctor.dname} 의사입니다.
+              <br /> 친절하고 꼼꼼하게 진료하겠습니다. <br />
+              *다이어트 포함 비급여 항목 확인해주세요
+            </div>
+          </div>
+          <div className="card-body border-bottom">
+            <h6>
+              <strong>병원위치</strong>
+            </h6>
+            <div className="card-text">{props.doctor.hospitalVO[0].hloc}</div>
+            <div className="detailMap"></div>
+          </div>
+          <div className="text-center revBtn">
+            <Link to={"/reservation/" + props.doctor.dnum}>
+              <button className="btn bg-primary btn-lg text-white mt-3 revBtn">
+                진료 예약하기
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="card detailCard border-0 p-2 mt-5">
-        <div className="card-body ">
-          <h5>리뷰 점수 1000 개</h5>
-        </div>
-      </div>
-      <Link to={"/reservation/" + props.doctor.dnum}>
-        <button className="btn bg-primary btn-lg text-white mt-3 revBtn">
-          진료 예약하기
-        </button>
-      </Link>
     </div>
   );
 };
--- springboot/frontend/src/component/Reservation.js
+++ springboot/frontend/src/component/Reservation.js
@@ -3,152 +3,84 @@ import "../css/reservation.css";
 
 const Reservation = () => {
   return (
-    <div className="container reservation">
-      <form>
-        <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label g-3">
-            성함
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleFormControlInput1"
-          />
-        </div>
-
-        <div className="mb-3">
+    <div className="d-flex justify-content-center revPage p-3">
+      <div className="card border-0 p-2">
+        <div className="card-body ">
+          <p className="h4 ps-2 mb-4">
+            <strong>예약자 정보</strong>
+          </p>
           <label for="exampleFormControlInput1" className="form-label">
-            주민등록번호
+            예약자 성명 : {localStorage.getItem("id")}
           </label>
-          <div className="input-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="주민번호 앞 6자리"
-            />
-            <span class="input-group-text">-</span>
-            <input
-              type="password"
-              class="form-control"
-              placeholder="주민번호 뒤 7자리"
-            />
-          </div>
+          <br />
+          <label for="exampleFormControlInput1" className="form-label mb-0">
+            휴대폰 번호 : {localStorage.getItem("num")}
+          </label>
         </div>
-        <div class="input-group mb-3"></div>
-
-        <div className="mb-3 symptom">
-          <label for="" className="form-label">
-            증상 선택
-          </label>
-          <div className="form-check">
+        <hr />
+        <div className="card-body ">
+          <p className="h4 ps-2 mb-4">
+            <strong>증상 선택</strong>
+          </p>
+          <div className="flex-start">
             <input
-              type="checkbox"
-              class="btn-check"
-              id="corona"
+              type="radio"
+              className="btn-check"
+              name="options"
+              id="option1"
               autocomplete="off"
             />
-            <label class="btn btn-sm btn-outline-primary" for="corona">
-              코로나
+            <label
+              className="btn btn-primary text-white me-2 mb-2"
+              for="option1"
+            >
+              Checked
             </label>
 
             <input
-              type="checkbox"
-              class="btn-check"
-              id="high"
+              type="radio"
+              className="btn-check"
+              name="options"
+              id="option2"
               autocomplete="off"
             />
-            <label class="btn btn-sm btn-outline-primary" for="high">
-              고혈압/고지혈증/당뇨
+            <label
+              className="btn btn-primary text-white me-2 mb-2"
+              for="option2"
+            >
+              Radio
             </label>
 
             <input
-              type="checkbox"
-              class="btn-check"
-              id="child"
+              type="radio"
+              className="btn-check"
+              name="options"
+              id="option3"
               autocomplete="off"
             />
-            <label class="btn btn-sm btn-outline-primary" for="child">
-              소아
+            <label
+              className="btn btn-primary text-white me-2 mb-2"
+              for="option3"
+            >
+              Disabled
             </label>
 
             <input
-              type="checkbox"
-              class="btn-check"
-              id="cold"
+              type="radio"
+              className="btn-check"
+              name="options"
+              id="option4"
               autocomplete="off"
             />
-            <label class="btn btn-sm btn-outline-primary" for="cold">
-              감기/호흡기
-            </label>
-
-            <input
-              type="checkbox"
-              class="btn-check"
-              id="skin"
-              autocomplete="off"
-            />
-            <label class="btn btn-sm btn-outline-primary" for="skin">
-              피부
-            </label>
-
-            <input
-              type="checkbox"
-              class="btn-check"
-              id="diet"
-              autocomplete="off"
-            />
-            <label class="btn btn-sm btn-outline-primary" for="diet">
-              다이어트
-            </label>
-
-            <input
-              type="checkbox"
-              class="btn-check"
-              id="woman"
-              autocomplete="off"
-            />
-            <label class="btn btn-sm btn-outline-primary" for="woman">
-              여성
-            </label>
-
-            <input
-              type="checkbox"
-              class="btn-check"
-              id="man"
-              autocomplete="off"
-            />
-            <label class="btn btn-sm btn-outline-primary" for="man">
-              남성
-            </label>
-
-            <input
-              type="checkbox"
-              class="btn-check"
-              id="hair"
-              autocomplete="off"
-            />
-            <label class="btn btn-sm btn-outline-primary" for="hair">
-              탈모
+            <label
+              className="btn btn-primary text-white me-2 mb-2"
+              for="option4"
+            >
+              Radio
             </label>
           </div>
         </div>
-
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">
-            증상 입력
-          </label>
-          <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="text-center">
-          <button type="submit" class="btn btn-lg btn-primary mt-3 ps-3 pe-3 ">
-            접수완료
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
