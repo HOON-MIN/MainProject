6d55b39f3225064bce3eda811fe3acacb089a56c
DESKTOP-4ROOTN0\kosmo <jinah0206@naver.com>
2022-09-23 21:42:45 +0900

    no message

--- springboot/src/main/java/com/main/ateam/member/controller/MemberController.java
+++ springboot/src/main/java/com/main/ateam/member/controller/MemberController.java
@@ -201,23 +201,23 @@ public class MemberController {
 		return "mypage/updateMypage";
 	}
 
-	@RequestMapping("/updateMypage")
+	@RequestMapping(value = "/updateMypage" ,method = RequestMethod.POST)
 	public String fileupLoad(FileVO v, HttpServletRequest request, MemberVO vo, HttpSession session) {
 		int num = 0;
 		num = (int) session.getAttribute("sessionNUM");
-		String img_path = "resources\\upload";
+		String img_path = "imgfile";
 		System.out.println("img_path :" + img_path);
 		String r_path = request.getRealPath("/");
 		System.out.println("r_path :" + r_path);
 		String oriFn = v.getFileOriName().getOriginalFilename();
 		System.out.println("oriFn : " + oriFn);
-
-		String path = "D:\\iKosmo113\\spring\\bootworkspace\\springboot\\src\\main\\resources\\static\\upload\\"
-				+ oriFn;
+		StringBuffer path = new StringBuffer();
+		path.append(r_path).append(img_path).append("\\");
+		path.append(oriFn);
 		vo.setNum(num);
 		vo.setProfimg(oriFn);
-		File f = new File(path);
-		System.out.println(f.getPath());
+		File f = new File(path.toString());
+		System.out.println("파일 위치" + f.getPath());
 		try {
 			v.getFileOriName().transferTo(f);
 		} catch (IllegalStateException | IOException e) {
@@ -225,43 +225,7 @@ public class MemberController {
 		}
 		memberService.memberUpdate(vo);
 		
-//		int num = 0;
-//		num = (int) session.getAttribute("sessionNUM");
-//		
-//		String img_path = "\\imgfile";
-//		//String r_path = request.getRealPath("/");
-//		String r_path = request.getSession().getServletContext().getRealPath("/");
-//		System.out.println("r_path: "+r_path);
-//		// Return the original filename
-//		String oriFn = v.getFileOriName().getOriginalFilename();
-//		long size = v.getFileOriName().getSize();
-//		String contentType = v.getFileOriName().getContentType();
-//		
-//		System.out.println("oriFn:"+oriFn);
-//		System.out.println("파일의 크기"+size);
-//		System.out.println("파일의 type:"+contentType);
-//		// 파일명에 아이디 추가
-//		String userID = (String) session.getAttribute("sessionID");
-//		
-//		StringBuffer path = new StringBuffer();
-//		path.append(r_path).append(img_path).append("\\");
-//		path.append(userID+"_");
-//		path.append(oriFn);
-//		System.out.println("FullPath:"+path);
-//		vo.setNum(num);
-//		vo.setProfimg(oriFn);
-//		File f = new File(path.toString());
-//		System.out.println("File f:"+f.getPath());
-//		try {
-//			v.getFileOriName().transferTo(f);
-//		} catch (IllegalStateException | IOException e) {
-//			e.printStackTrace();
-//		}
-//		System.out.println("파일 테스트");
-//		System.out.println("num"+num);
-//		memberService.memberUpdate(vo);
-		
-		return "member/memberMypage";
+		return "redirect:memberMypage";
 	}
 
 	@ResponseBody
