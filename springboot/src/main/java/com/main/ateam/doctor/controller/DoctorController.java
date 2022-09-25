a409de619e2285db09e3ceb3efe1367a75ccccbf
DESKTOP-HOON\hunma <hunman90@naver.com>
2022-09-26 00:09:27 +0900

    디자인 리스트 출력

--- springboot/src/main/java/com/main/ateam/doctor/controller/DoctorController.java
+++ springboot/src/main/java/com/main/ateam/doctor/controller/DoctorController.java
@@ -26,15 +26,92 @@ import com.main.ateam.vo.DoctorVO;
 import com.main.ateam.vo.HospitalVO;
 import com.main.ateam.vo.MemberVO;
 import com.main.ateam.vo.ReserveVO;
+import com.main.ateam.vo.SearchVO;
 @Controller
 @RequestMapping(value = "/doctor")
 public class DoctorController {
+	
+	private int nowPage = 1; // 현재 페이지 값 //**
+	private int nowBlock = 1; // 현재 블럭
+	private int totalRecord = 0; // 총 게시물 수
+	private int numPerPage = 6; // 한페이지당 보여질 게시물 수
+	private int pagePerBlock = 5; //한 블럭당 보여질 페이지 수 //**
+	private int totalPage =0; // 전체 페이지 수 -> totalRecord/numPerPage //**
+	private int totalBlock =0; // 전체 블럭 수
+	private int beginPerPage =0; // 각 페이지별 시작 게시물의 index값
+	private int endPerPage =0; // 각 페이지별 마지막 게시물의 index값
+
 
 	@Autowired
 	private DoctorService doctorservice;
 	
 	@Autowired
 	private HospitalService hospitalService;
+	
+	
+	@RequestMapping("doctorList")
+	public String doctorListPage(SearchVO svo, Model model, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("---------- hospControll ----------");
+		System.out.println("시작페이지 : "+svo.getBeginPerPage());
+		System.out.println("마지막페이지 : "+svo.getEndPerPage());
+		System.out.println("검색 : "+svo.getSearch());
+		System.out.println("분류 : "+svo.getCategory());
+		
+		List<DoctorVO> dcategory = doctorservice.doctorCategorySpring();
+		
+		totalRecord = doctorservice.doctorCnt(svo);
+		totalPage = (int) Math.ceil(totalRecord / (double) numPerPage                          );
+		totalBlock = (int) Math.ceil((double) totalPage / pagePerBlock);
+		
+		if (svo.getSearchreset().equals("1")) {
+			System.out.println("cPage hosp 리셋 =>"+ svo.getcPage());
+			nowPage = Integer.parseInt(svo.getcPage());
+		}else {
+			System.out.println("cPage hosp 페이지 번호 선택시 =>"+svo.getcPage());
+			nowPage = Integer.parseInt(svo.getcPage());
+		}
+		
+		beginPerPage = (nowPage-1)*numPerPage + 1;
+		endPerPage = (beginPerPage-1)+numPerPage;
+		
+		svo.setBeginPerPage(beginPerPage);
+		svo.setEndPerPage(endPerPage);
+		svo.setCategory(request.getParameter("category"));
+		
+		List<DoctorVO> dlist = doctorservice.doctorListSpring(svo);
+		System.out.println("hospCont => "+dlist.isEmpty());
+	
+		
+		int startPage = (int)((nowPage-1)/pagePerBlock)*pagePerBlock+1;
+		int endPage = startPage+pagePerBlock-1;
+		if(endPage > totalPage) {
+			endPage = totalPage;
+		}
+		
+
+		model.addAttribute("dcategory",dcategory);
+		model.addAttribute("dlist", dlist);
+		model.addAttribute("category", svo.getCategory());
+		model.addAttribute("search", svo.getSearch());
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("pagePerBlock", pagePerBlock);
+		model.addAttribute("totalPage", totalPage);
+		
+		System.out.println("totalRecord :"+ totalRecord);
+		System.out.println("startPage :"+ startPage);
+		System.out.println("endPage :"+ endPage);
+		System.out.println("nowPage :"+ nowPage);
+		System.out.println("pagePerBlock :"+ pagePerBlock);
+		System.out.println("totalPage :"+ totalPage);
+
+		System.out.println("----------------------------");
+		
+		return "doctor/doctorList";
+		
+	}
 	
 	@PostMapping("/doctorLogin")
 	public ModelAndView doctorLogin(HttpSession session, DoctorVO vo) {
